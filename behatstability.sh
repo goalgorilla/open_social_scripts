bbac4a34601e96fdd2134fcb979513bd0f9154fb
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2017-07-17 10:40:02 +0200

    DS-3830 by frankgraave, bramtenhove: Add ability to run multiple tags using the behat test script

--- behatstability.sh
+++ behatstability.sh
@@ -1,9 +1,33 @@
 #!/usr/bin/env bash
 
+# Take optional arguments for this script.
+#
+# To run all stability tests except for DS-2082 and DS-816:
+# /behatstability.sh
+# To run specified tags except for DS-2082 and DS-816:
+# /behatstability.sh stability-1 stability-2 stability-5
+if [ -z "$1" ];
+then
+  TAGS="stability&&~DS-2082&&~DS-816"
+else
+  for var in "$@"
+  do
+    if [ -z "$TAGS" ];
+    then
+      TAGS="$var"
+    else
+      TAGS="$TAGS,$var"
+    fi
+  done
+  TAGS="$TAGS&&~DS-2082&&~DS-816"
+fi
+
+echo $TAGS;
+
 PROJECT_FOLDER=/var/www/html/profiles/contrib/social/tests/behat
 
 /var/www/vendor/bin/behat --version
 
 echo $PROJECT_FOLDER/config/behat.yml;
 
-/var/www/vendor/bin/behat $PROJECT_FOLDER --config $PROJECT_FOLDER/config/behat.yml --tags "stability&&~DS-2082&&~DS-816"
+/var/www/vendor/bin/behat $PROJECT_FOLDER --config $PROJECT_FOLDER/config/behat.yml --tags $TAGS
