9ae33b0fa6a205ba07ab94d2484d7ce2b5d586b1
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2017-07-18 15:10:43 +0200

    Revert "DS-3830 by bramtenhove: fix issue with private files on running update script"
    This reverts commit bc7f0cb22480f7233e6909caec4f66b05efa8239.

--- ci/restore-permissions.sh
+++ ci/restore-permissions.sh
@@ -2,8 +2,6 @@
 
 ls -lah html/sites/default
 sudo chown -R travis:travis html
-sudo chown -R travis:travis files_private
 chmod 755 html/sites/default
 chmod 755 html/sites/default/*
 chmod -R 777 html/sites/default/files
-chmod -R 777 files_private
