52df3b131a0aabfb3c98a897d5cb12cff7bbe197
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2017-08-15 10:38:27 +0200

    DS-3830 by bramtenhove: For the update we use the branch

--- ci/build-install-update.sh
+++ ci/build-install-update.sh
@@ -2,12 +2,10 @@
 
 set -ev
 
-composer require goalgorilla/open_social:1.0.0
-
 # Install the site in version X.
 docker exec -i social_ci_web bash /var/www/scripts/social/install/install_script.sh
 bash scripts/social/ci/restore-permissions.sh
 # Update composer to version Y.
-rm -f composer.lock && composer require goalgorilla/open_social:"dev-8.x-1.x" --update-with-dependencies
+rm -f composer.lock && composer require goalgorilla/open_social:dev-${2}#${3} --update-with-dependencies
 bash scripts/social/ci/restore-permissions.sh
 docker exec -i social_ci_web bash /var/www/scripts/social/install/update.sh
