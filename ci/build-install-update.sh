55a596c75653ddba7f37caf6f09ec85379ad66f6
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2017-08-15 09:54:51 +0200

    Revert "DS-3830 by bramtenhove: Install the 1.2.0 version when testing the installation"
    This reverts commit f00700fd6c08727a290465af86bd5e1928ea79c0.

--- ci/build-install-update.sh
+++ ci/build-install-update.sh
@@ -8,6 +8,6 @@ composer require goalgorilla/open_social:1.0.0
 docker exec -i social_ci_web bash /var/www/scripts/social/install/install_script.sh
 bash scripts/social/ci/restore-permissions.sh
 # Update composer to version Y.
-rm -f composer.lock && composer require goalgorilla/open_social:1.2.0
+rm -f composer.lock && composer require goalgorilla/open_social:"dev-8.x-1.x"
 bash scripts/social/ci/restore-permissions.sh
 docker exec -i social_ci_web bash /var/www/scripts/social/install/update.sh
