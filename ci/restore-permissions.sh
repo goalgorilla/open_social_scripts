e69e5a9c1f81e167596f1e40da42f4bd8949e47f
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2017-08-15 11:51:53 +0200

    DS-3830 by bramtenhove: Added files_private to the folders we restore the permissions of

--- ci/restore-permissions.sh
+++ ci/restore-permissions.sh
@@ -2,6 +2,8 @@
 
 ls -lah html/sites/default
 sudo chown -R travis:travis html
+sudo chown -R travis:travis files_private
 chmod 755 html/sites/default
 chmod 755 html/sites/default/*
 chmod -R 777 html/sites/default/files
+chmod -R 777 files_private
