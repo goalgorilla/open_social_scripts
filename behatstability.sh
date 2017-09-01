1364cdb9aa534ce007aade8d62dbde3f1953b9b3
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2017-09-01 10:28:16 +0200

    Revert "Revert "Merge pull request #12 from goalgorilla/feature/2840680-fix-failing-behat""
    This reverts commit d2c37f0f0ac0b449bac88e08b6879a04314bac8d.

--- behatstability.sh
+++ behatstability.sh
@@ -2,13 +2,13 @@
 
 # Take optional arguments for this script.
 #
-# To run all stability tests except for DS-2082:
+# To run all stability tests:
 # /behatstability.sh
-# To run specified tags except for DS-2082:
+# To run specified tags:
 # /behatstability.sh stability-1 stability-2 stability-5
 if [ -z "$1" ];
 then
-  TAGS="stability&&~DS-2082"
+  TAGS="stability"
 else
   for var in "$@"
   do
@@ -19,7 +19,7 @@ else
       TAGS="$TAGS,$var"
     fi
   done
-  TAGS="$TAGS&&~DS-2082"
+  TAGS="$TAGS"
 fi
 
 PROJECT_FOLDER=/var/www/html/profiles/contrib/social/tests/behat
