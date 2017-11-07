924796f432b7701740747b6384e0bd10bdc66d63
jochemvn <jochem@goalgorilla.com>
2017-11-07 14:03:37 +0100

    DS-4398 by jochemvn, bramtenhove: Ignore PR default template

--- check-coding-standards.sh
+++ check-coding-standards.sh
@@ -14,12 +14,11 @@ fi
 
 # Run PHP Code Sniffer.
 echo "Running PHP Code Sniffer with Drupal sniffer."
-/var/www/vendor/squizlabs/php_codesniffer/scripts/phpcs $REPORT --standard=Drupal --extensions=php,module,inc,install,test,profile,theme /var/www/html/profiles/contrib/social --ignore=*/node_modules/* --ignore=*.css --ignore=social.info.yml
-
+/var/www/vendor/squizlabs/php_codesniffer/scripts/phpcs $REPORT --standard=Drupal --extensions=php,module,inc,install,test,profile,theme /var/www/html/profiles/contrib/social --ignore=*/node_modules/* --ignore=*.css --ignore=social.info.yml --ignore=.github/PULL_REQUEST_TEMPLATE.md
 
 # Ignore warnings on exit for DrupalPractice.
 /var/www/vendor/squizlabs/php_codesniffer/scripts/phpcs --config-set ignore_warnings_on_exit 1
 
 # Run PHP Code Sniffer.
 echo "Running PHP Code Sniffer with DrupalPractice sniffer."
-/var/www/vendor/squizlabs/php_codesniffer/scripts/phpcs $REPORT --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme /var/www/html/profiles/contrib/social --ignore=*/node_modules/* --ignore=*.css --ignore=social.info.yml
+/var/www/vendor/squizlabs/php_codesniffer/scripts/phpcs $REPORT --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme /var/www/html/profiles/contrib/social --ignore=*/node_modules/* --ignore=*.css --ignore=social.info.yml --ignore=.github/PULL_REQUEST_TEMPLATE.md
