9a87a63b5db536da9d12e65e920b8980c9c25f33
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2018-06-29 16:31:29 +0200

    Make sure it doesn't matter if you run the makefile script on social_web or social_ci_web container

--- ci/build-social-make.sh
+++ ci/build-social-make.sh
@@ -2,8 +2,10 @@
 
 set -ev
 
+CONTAINER=`docker ps --format '{{.Names}}' | grep "\w[social]\w[web]"`
+
 # Restore permissions and do the make install.
 mkdir html/sites/default/files
 bash scripts/social/ci/restore-permissions.sh
 bash scripts/social/ci/drush-make-install.sh
-docker exec -i social_ci_web bash /var/www/scripts/social/install/install_script.sh
+docker exec -i $CONTAINER bash /var/www/scripts/social/install/install_script.sh
