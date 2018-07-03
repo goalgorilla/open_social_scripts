6602a3678618cc3f1837c21e50ef4b25c91e085e
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2018-07-03 13:06:46 +0200

    Revert "Make sure it doesn't matter if you run the update script on social_web or social_ci_web container"
    This reverts commit 69cfe2c8f8a551a6dc48be97e2d8ee1605e572ac.

--- ci/build-install-update.sh
+++ ci/build-install-update.sh
@@ -2,14 +2,12 @@
 
 set -ev
 
-CONTAINER=`docker ps --format '{{.Names}}' | grep "\w[social]\w[web]"`
-
 # Install the site in version X.
-docker exec -i $CONTAINER bash /var/www/scripts/social/install/install_script.sh
+docker exec -i social_ci_web bash /var/www/scripts/social/install/install_script.sh
 bash scripts/social/ci/restore-permissions.sh
 # Update composer to version Y.
 rm -f composer.lock && composer require goalgorilla/open_social:dev-${2}#${3} --no-update
 # do this anyway
 composer update
 bash scripts/social/ci/restore-permissions.sh
-docker exec -i $CONTAINER bash /var/www/scripts/social/install/update.sh
+docker exec -i social_ci_web bash /var/www/scripts/social/install/update.sh
