83ed047af3a5b297f2123dd8df6ce2e4fb44eac3
jochemvn <jochem@goalgorilla.com>
2018-07-19 14:13:44 +0200

    Skip generaion of demo content on CI

--- ci/build-install-update.sh
+++ ci/build-install-update.sh
@@ -3,7 +3,7 @@
 set -ev
 
 # Install the site in version X.
-docker exec -i social_ci_web bash /var/www/scripts/social/install/install_script.sh
+docker exec -i social_ci_web bash /var/www/scripts/social/install/install_script.sh --skip-content
 bash scripts/social/ci/restore-permissions.sh
 # Update composer to version Y.
 rm -f composer.lock && composer require goalgorilla/open_social:dev-${2}#${3} --no-update
--- ci/build-social-make.sh
+++ ci/build-social-make.sh
@@ -6,4 +6,4 @@ set -ev
 mkdir html/sites/default/files
 bash scripts/social/ci/restore-permissions.sh
 bash scripts/social/ci/drush-make-install.sh
-docker exec -i social_ci_web bash /var/www/scripts/social/install/install_script.sh
+docker exec -i social_ci_web bash /var/www/scripts/social/install/install_script.sh --skip-content
