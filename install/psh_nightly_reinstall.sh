ec989d26ee38fa877037ead5a395af8400fc92a0
jaapjan <mail@jaapjan.nl>
2019-09-24 14:30:48 +0200

    Install Redis as well.

--- install/psh_nightly_reinstall.sh
+++ install/psh_nightly_reinstall.sh
@@ -8,7 +8,7 @@ echo "RESETTING OPCACHE"
 php -r 'opcache_reset();';
 
 echo "ENABLING MULTIPLE SOCIAL EXTENSIONS"
-drush --root=/app/web -y pm-enable social_book social_comment_upload social_event_an_enroll social_event_type social_gdpr social_group_quickjoin social_landing_page social_sharing social_tagging social_user_export social_embed social_private_message social_profile_fields social_profile_organization_tag social_profile_privacy social_lazy_loading social_lazy_loading_images
+drush --root=/app/web -y pm-enable social_book social_comment_upload social_event_an_enroll social_event_type social_gdpr social_group_quickjoin social_landing_page social_sharing social_tagging social_user_export social_embed social_private_message social_profile_fields social_profile_organization_tag social_profile_privacy social_lazy_loading social_lazy_loading_images redis
 
 echo "ENABLING DEMO CONTENT MODULE"
 drush --root=/app/web -y pm-enable social_demo
