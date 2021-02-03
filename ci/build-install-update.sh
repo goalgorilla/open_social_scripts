9dbbc791e21472b0196853b0315cbdebb6f1c69e
ronaldtebrake <ronald@goalgorilla.com>
2021-02-03 20:03:13 +0100

    Ensure we also install social search autocomplete when doing install update and composer require

--- ci/build-install-update.sh
+++ ci/build-install-update.sh
@@ -7,6 +7,8 @@ docker exec -i social_ci_web bash /var/www/scripts/social/install/install_script
 bash scripts/social/ci/restore-permissions.sh
 # Update composer to version Y.
 rm -f composer.lock && composer require goalgorilla/open_social:dev-${2}#${3} --update-with-all-dependencies
+# ALso we install from 8.x with search autocomplete enabled, so we need to composer require it to make sure it doesn't break travis.
+composer require drupal/social_search_autocomplete
 # do this anyway
 composer update
 bash scripts/social/ci/restore-permissions.sh
