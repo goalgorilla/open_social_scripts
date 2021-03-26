8d69a89d1c4d4f33ff97810b4997b3207e6be395
Alexander Varwijk <Kingdutch@users.noreply.github.com>
2021-03-26 14:00:38 +0100

    Allow Travis to determine target update

--- ci/build-install-update.sh
+++ ci/build-install-update.sh
@@ -6,7 +6,7 @@ set -ev
 docker exec -i social_ci_web bash /var/www/scripts/social/install/install_script.sh
 bash scripts/social/ci/restore-permissions.sh
 # Update composer to version Y.
-rm -f composer.lock && composer require goalgorilla/open_social:dev-${2}#${3} --update-with-all-dependencies
+composer require ${1} --update-with-all-dependencies
 # ALso we install from 8.x with search autocomplete enabled, so we need to composer require it to make sure it doesn't break travis.
 composer require drupal/social_search_autocomplete
 # do this anyway
