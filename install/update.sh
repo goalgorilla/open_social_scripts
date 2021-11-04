793d2c0158fa5f18ec2db79b2fdb4249e99cea4f
ronaldtebrake <ronald@goalgorilla.com>
2021-11-04 09:07:10 +0100

    Enable variationcache on updates
    See #3134690, Group now relies on variationcache and it needs to be enabled before we update. Even if there is an update hook added, we can't add requirements because regardless of which version users are coming from they need it enabled first before they continue updating.

--- install/update.sh
+++ install/update.sh
@@ -5,5 +5,9 @@ set -e
 cd /var/www/html/;
 
 drush -y cr
+## Needed for updates from rc-5 -> 1.4.0 of Group see: #3134690
+## It is fired in an update hook, however we can't know which order it's run or which version updates are coming from
+## so we need to enable it first otherwise we are running in to errors.
+drush -y variationcache
 drush -y updatedb
 
