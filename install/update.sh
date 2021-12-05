1ceaaaec10147c1022c2028e3f2cf719be7df86a
ronaldtebrake <ronald@goalgorilla.com>
2021-12-05 17:48:07 +0100

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -8,5 +8,5 @@ drush -y cr
 ## Needed for updates from rc-5 -> 1.4.0 of Group see: #3134690
 ## It is fired in an update hook, however we can't know which order it's run or which version updates are coming from
 ## so we need to enable it first otherwise we are running in to errors.
-drush -y variationcache
+drush -y en variationcache
 drush -y updatedb
