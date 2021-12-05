21cbf0b5766b726df5ec5e21cc8058c41f43323e
ronaldtebrake <ronald@goalgorilla.com>
2021-12-05 17:29:21 +0100

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -4,10 +4,9 @@ set -e
 # Script to update Drupal in the docker container.
 cd /var/www/html/;
 
+drush -y cr
 ## Needed for updates from rc-5 -> 1.4.0 of Group see: #3134690
 ## It is fired in an update hook, however we can't know which order it's run or which version updates are coming from
 ## so we need to enable it first otherwise we are running in to errors.
 drush -y variationcache
-drush -y cr
 drush -y updatedb
-
