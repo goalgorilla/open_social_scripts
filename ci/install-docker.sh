eb2cae71708b559cb531833c36bfd38245c3bab3
navneet0693 <navneetsingh0693@gmail.com>
2022-06-07 16:54:15 +0530

    Updated docker installation steps to install latest docker and docker-compose tools.

--- ci/install-docker.sh
+++ ci/install-docker.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+# See: https://docs.travis-ci.com/user/docker/#using-docker-compose
+# See: https://docs.travis-ci.com/user/docker/#installing-a-newer-docker-version
+# See: https://docs.docker.com/engine/install/ubuntu/#next-steps
 sudo apt-get clean
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
+sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# Print docker and docker compose tool versions.
 docker --version
-sudo rm /usr/local/bin/docker-compose || true
-curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > docker-compose
-chmod +x docker-compose
-sudo mv docker-compose /usr/local/bin
-docker-compose --version
+docker compose version
